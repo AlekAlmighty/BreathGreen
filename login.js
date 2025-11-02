54d69ab136960deb79aa8eac62c10b42b706a53f
AlekAlmighty <148012564+AlekAlmighty@users.noreply.github.com>
2025-11-02 23:15:10 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+const container = document.getElementById('container');
+const switchToRegister = document.getElementById('switchToRegister');
+const switchToLogin = document.getElementById('switchToLogin');
+
+switchToRegister.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.classList.add('active');
+});
+
+switchToLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.classList.remove('active');
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  window.location.href = "profile/dashboard/dashboard.html";
+});
